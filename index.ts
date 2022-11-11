4a6626f20107525420907a532045604b1c243144
nuruzi <39434080+nuruzi@users.noreply.github.com>
2022-11-11 12:21:41 +0330

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import './style.css';
 
-const appDiv: HTMLElement = document.getElementById('app'); appDiv.innerHTML = `<h2>Adapter Pattern<h2/>`;
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h2>Adapter Pattern<h2/>`;
 
 // =============== Adapter Pattern ===============
 
@@ -14,14 +15,14 @@ interface Android {
 
 class IPhoneXs implements IPhone {
   useLightning() {
-    console.log("Using lightning port..");
+    console.log('Using lightning port..');
     appDiv.innerHTML += `<h4>Using lightning port..</h4>`;
   }
 }
 
 class GooglePixel3 implements Android {
   useUSBc() {
-    console.log("Using USB-C port..");
+    console.log('Using USB-C port..');
     appDiv.innerHTML += `<h4>Using USB-C port..</h4>`;
   }
 }
